df92caaf20f2d3bdab3dc1260895a434a78fa68f
GibranVasquez <158479829+GibranVasquez@users.noreply.github.com>
2025-05-09 14:52:27 -0600

    Act6-Creando Metodo Put en el controller

--- src/main/java/com/Gibran/Alumnos/Controllers/AlumnoController.java
+++ src/main/java/com/Gibran/Alumnos/Controllers/AlumnoController.java
@@ -3,8 +3,11 @@ package com.Gibran.Alumnos.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,22 @@ public class AlumnoController {
    @PostMapping("/insertar-alumnos")
     public AlumnoModel InsertarAlumno(@RequestBody AlumnoModel alumno) {
         return alumnoRepository.save(alumno);
+    }
+
+    //Metodo para Editar un alumno a la base de datos
+    @PutMapping("/editar-alumnos/{id}")
+    public ResponseEntity<AlumnoModel> actualizarAlumno(@PathVariable Long id, @RequestBody AlumnoModel alumno) {
+        return alumnoRepository.findById(id).map(alumnoExistente -> {
+            alumnoExistente.setNombre(alumno.getNombre());
+            alumnoExistente.setApellido(alumno.getApellido());
+            alumnoExistente.setEmail(alumno.getEmail());
+            alumnoExistente.setTelefono(alumno.getTelefono());
+            alumnoExistente.setNumeroControl(alumno.getNumeroControl());
+            alumnoExistente.setCarrera(alumno.getCarrera());
+            alumnoExistente.setImagenURL(alumno.getImagenURL());
+            AlumnoModel actualizado = alumnoRepository.save(alumnoExistente);
+            return ResponseEntity.ok(actualizado);
+        }).orElse(ResponseEntity.notFound().build());
     }
 
 
