377eda9c4c51a6b53cf400fd9a92b66f162f13f8
GibranVasquez <158479829+GibranVasquez@users.noreply.github.com>
2025-05-09 10:35:51 -0600

    Act5-Creando Metodo Post en el controlador

--- src/main/java/com/Gibran/Alumnos/Controllers/AlumnoController.java
+++ src/main/java/com/Gibran/Alumnos/Controllers/AlumnoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,18 @@ public class AlumnoController {
 
     @Autowired
     private  AlumnoRepository alumnoRepository;
-
+//Metodo GET para obtener todos los alumnos
     @GetMapping("/traer-alumnos")
     public List<AlumnoModel> TraerAlumnos() {
         return alumnoRepository.findAll();
     }
+
+    //Metodo para insertar un alumno a la base de datos
+   @PostMapping("/insertar-alumnos")
+    public AlumnoModel InsertarAlumno(@RequestBody AlumnoModel alumno) {
+        return alumnoRepository.save(alumno);
+    }
+
+
 }
+
